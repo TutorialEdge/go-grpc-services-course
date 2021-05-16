b0af373658c132558b0f3492770eb43b08fc2440
Elliot Forbes <emforce@live.com>
2021-05-16 11:29:02 +0100

    Sun May 16 11:29:02 BST 2021

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,11 +2,21 @@ package main
 
 import (
 	"log"
+
+	"github.com/TutorialEdge/go-grpc-services-course/internal/db"
+	"github.com/TutorialEdge/go-grpc-services-course/internal/rocket"
 )
 
 func Run() error {
 	// responsible for initializing and starting
 	// our gRPC server
+	rocketStore, err := db.New()
+	if err != nil {
+		return err
+	}
+
+	_ = rocket.New(rocketStore)
+
 	return nil
 }
 
---
+++ internal/db/db.go
@@ -0,0 +1,53 @@
+package db
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/TutorialEdge/go-grpc-services-course/internal/rocket"
+	"github.com/jmoiron/sqlx"
+)
+
+type Store struct {
+	db *sqlx.DB
+}
+
+// New - returns a new store or error
+func New() (Store, error) {
+	dbUsername := os.Getenv("DB_USERNAME")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbTable := os.Getenv("DB_TABLE")
+	dbPort := os.Getenv("DB_PORT")
+	dbSSLMode := os.Getenv("DB_SSL_MODE")
+
+	connectionString := fmt.Sprintf(
+		"host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
+		dbHost,
+		dbPort,
+		dbUsername,
+		dbTable,
+		dbPassword,
+		dbSSLMode,
+	)
+
+	db, err := sqlx.Connect("postgres", connectionString)
+	if err != nil {
+		return Store{}, err
+	}
+	return Store{
+		db: db,
+	}, nil
+}
+
+func (s Store) GetRocketByID(id string) (rocket.Rocket, error) {
+	return rocket.Rocket{}, nil
+}
+
+func (s Store) InsertRocket(rkt rocket.Rocket) (rocket.Rocket, error) {
+	return rocket.Rocket{}, nil
+}
+
+func (s Store) DeleteRocket(id string) error {
+	return nil
+}
